de58559489315b13e4f70e21f77fe7d0e22405bf
ImrankhanDev <imrankhantech707@gmail.com>
2021-10-15 11:07:09 +0600

    added new features

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import './Cart.css'
+import React from "react";
+import  './Cart.css'
 const Cart = (props) => {
-    const{cart}=props ||{}
+  const { cart } = props || {};
 
-// reduce method
-const totalReducer =(prevValue,currentValue)=>prevValue+currentValue.price;
-const total = cart.reduce(totalReducer,0).toFixed(2)
-const tax = (0.1*total).toFixed(2);
-const totalPrice = parseFloat(total+tax).toFixed(2);
+  // reduce method
+  const totalReducer = (prevValue, currentValue) =>
+    prevValue + currentValue.price;
+  const total = cart.reduce(totalReducer, 0).toFixed(2);
+  const tax = (0.1 * total).toFixed(2);
+  const totalPrice = parseFloat(total + tax).toFixed(2);
 
- console.log(total);
-    return (
-        <div>
-            {/* <h3>THIS hello from Cart</h3>
-             */}
-             <h2><i class="fas fa-cart-plus fa-1x"> Cart :  {cart.length}</i></h2>
-            <h2>Price : {total}</h2>
-            <h2>Tax : {tax}</h2>
-            <h3>Total price : {totalPrice}</h3>
+  console.log(total);
+  return (
+    <div className="cart">
+      <h2>
+        <i class="fas fa-cart-plus fa-1x"> Cart : {cart.length}</i>
+      </h2>
+      <h2>Price : {total}</h2>
+      <h2>Tax : {tax}</h2>
+      <h3>Total price : {totalPrice}</h3>
 
-   <ul>
-       {
-           cart.map(product=> <li>{product.name}</li>
-           )
-       }
-   </ul>
-        </div>
-    );
+      <ul>
+        {cart.map((product) => (
+          <li>{product.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Cart;
